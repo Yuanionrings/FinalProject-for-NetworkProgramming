a17688d6e72b7a37737866e327363d5b2c93fdb9
Yuan Gao <43865485+Yuanionrings@users.noreply.github.com>
2019-11-22 21:47:44 -0500

    Add files via upload

---
+++ ScreenShare.java
@@ -0,0 +1,28 @@
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+public class ScreenShare {
+
+}
