785a4e0e75918920f6ed906dee257df6bd0debeb
Yuan Gao <yuanionring@gmail.com>
2019-12-03 00:27:07 -0500

    Fully working screen casting
    minor bugs need fix (cursor is just a black block)

--- src/ScreenCast.java
+++ src/ScreenCast.java
@@ -1,6 +1,13 @@
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.Robot;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -8,7 +15,10 @@ import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
@@ -25,19 +35,56 @@ public class ScreenCast {
 
 		new ScreenShare().interactive();
 	}
+	
+	private boolean mouseMove;
 
 	/*
 	 * Decide if user is host or client
 	 */
 	private void interactive() {
 		Scanner s = new Scanner(System.in);
-		
 		while (true) {
-			InetAddress addr = InetAddress.getByName("localhost");
-			System.out.print(addr.getCanonicalHostName() + ">>> ");
-			
-			String temp = s.nextLine();
+			try {
+				InetAddress addr = InetAddress.getByName("localhost");
+				System.out.print(addr.getCanonicalHostName() + ">>> ");
+				if (s.hasNext()) {
+					intepretCommand(s.nextLine());
+				}
+
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
 		}
+	}
+	
+	/**
+	 * Helper class for intepreting inputs
+	 * 
+	 * @param cmd
+	 */
+	private void intepretCommand(String cmd) {
+		StringTokenizer tokenizer = new StringTokenizer(cmd);
+		String commandToken = tokenizer.nextToken();
+		if (commandToken.equals("server")) {
+			String port = tokenizer.nextToken();
+			server(Integer.parseInt(port));
+		} else if (commandToken.equals("client")) {
+			String serverAddr = tokenizer.nextToken();
+			String port = tokenizer.nextToken();
+			client(serverAddr, Integer.parseInt(port));
+		} else if (commandToken.equals("close")){
+			close();
+		} else if (commandToken.equals("mousemove")){
+			this.mouseMove= true;
+		} else if (commandToken.equals("nomousemove")){
+			this.mouseMove= false;
+		} else {
+			System.out.println("Unrecognized Command");
+		}
+
 	}
 	
 	/*
@@ -45,44 +92,76 @@ public class ScreenCast {
 	 */
 	private void client(String serverAddr, int port) {
 		JFrame frame = new JFrame();
-		ImagePanel panel = new ImagePanel();
+		Pictures picture = new Pictures();
 		frame.setResizable(true);
 		frame.add(panel);
 		frame.pack();
 		frame.setVisible(true);
-		
-		while (true) {
-			socket = new Socket(serverAddr, port);
-			ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-			image = ImageIO.read(inputStream);
+		try {
+			BufferedImage image;
+			Robot r = new Robot();
+			long startTime = System.currentTimeMillis();
+			Random rand = new Random();
+			while(true){
+				if(this.mouseMove && System.currentTimeMillis() - startTime > 5000){
+					r.mouseMove(rand.nextInt(200), rand.nextInt(200));
+					startTime = System.currentTimeMillis();
+				}
+
+				socket = new Socket(serverAddr, port);
+				ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+				image = ImageIO.read(inputStream);
+				panel.setImg(image);
+				panel.repaint();
+				socket.close();
+			}
+
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		//TODO fix recieving host screen and re-construct
-		
-		//TODO client being able to perform actions on hosts screen
 	}
+	
+	ServerSocket  server;
+	Socket socket;
 	
 	/*
 	 * Handles Server side interactions
 	 */
 	private void server(int port) {
-		server = new ServerSocket(port);
-		
-		while(true){
-			socket = server.accept();
-			InetAddress addr = socket.getInetAddress();
-			System.out.println("Received Connection From " + addr.getCanonicalHostName() + " at " + addr.getHostAddress());
-			
-			
-			ObjectOutputStream outstream = new ObjectOutputStream(socket.getOutputStream());
-			BufferedImage img;
-			
-			
-			//TODO fix outputing host screen
-			
-			//TODO recieve client mouse/keyboard input and execute on hosts screen
-			
-		}
+		try {
+			server = new ServerSocket(port);
+			Robot r = new Robot();
+
+			while(true){
+				try{
+					socket = server.accept();
+					InetAddress addr = socket.getInetAddress();
+					System.out.println(
+							"Received Connection From " + addr.getCanonicalHostName()
+									+ " at " + addr.getHostAddress());
+					ObjectOutputStream outstream = new ObjectOutputStream(socket.getOutputStream());
+					BufferedImage img;
+					img = r.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+					Point mouse = MouseInfo.getPointerInfo().getLocation();
+					Graphics g = img.getGraphics();
+					g.setColor(Color.BLACK);
+					g.fillRect(mouse.x, mouse.y, 30, 30);
+					ImageIO.write(img, "jpg", outstream);
+					socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}  catch (AWTException e) {
+			e.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} 
 	}
 	
 	/*
@@ -103,7 +182,11 @@ public class ScreenCast {
 	 * Closes current conection
 	 */
 	private void close(){
-		socket.close();
-		server.close();
+		try {
+			socket.close();
+			server.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
